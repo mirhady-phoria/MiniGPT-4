5899691773f05f5a85a525fd6b4a2d31d85a78b9
Mirhady Dorodjatun <mirhady@phoria.com.au>
2023-10-25 02:28:17 +1100

    update manifest

--- setup.py
+++ setup.py
@@ -83,9 +83,9 @@ if __name__ == "__main__":
     setup(
         name='minigpt4',
         version='0.1',
-        packages=find_packages(),
+        packages=find_packages().append('configs'),
         include_package_data=True, # this will look on MANIFEST.in for reference
-        package_data={'minigpt4': ['configs/default.yaml, configs/models/minigpt4_vicuna0.yaml, eval_configs/minigpt_eval.yaml']},
+        package_data={'minigpt4': ['configs/default.yaml, configs/models/minigpt4_vicuna0.yaml']},
         install_requires=parse_requirements("requirements.txt"),
         url='https://github.com/mirhady-phoria/MiniGPT-4',
         author='Jun Chen, Deyao Zhu, Xiaoqian Shen, Xiang Li, Zechun Liu, Pengchuan Zhang, Raghuraman Krishnamoorthi, Vikas Chandra, Yunyang Xiong☨, Mohamed Elhoseiny☨',
